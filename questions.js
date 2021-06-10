96edac862766e9f409b466ceef30442d1b2934cc
anamolacharya <anamol222@gmailcom>
2021-06-10 13:11:08 -0500

    Question answer

--- questions.js
+++ questions.js
@@ -14,3 +14,26 @@ const ask = (i = 0) => {
 };
 
 ask();
+
+const answers = [];
+process.stdin.on("data", (data) => {
+  answers.push(data.toString().trim());
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    process.exit();
+  }
+});
+
+process.on("exit", () => {
+  const [name, location, profession] = answers;
+
+  console.log(`
+
+    Thank you for your answers.
+
+    ${name} is a ${profession} from ${location}!!!
+
+
+    `);
+});
